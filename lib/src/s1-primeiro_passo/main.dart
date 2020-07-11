64fb328222b431ac18cd5493a2392bc2ed80c667
Jaelson Santos <jaelson.jua@gmail.com>
2020-07-11 13:15:59 -0300

    segundo commit

--- lib/src/s1-primeiro_passo/main.dart
+++ lib/src/s1-primeiro_passo/main.dart
@@ -1,9 +1,3 @@
-
-import 'package:intl/intl.dart';
-
 void main(List<String> arguments) {
-  print('teste');
-  DateTime date = DateTime.now();
-  print(new DateFormat('dd-MM-yyyy').format(new DateTime.now()));
-  print(date);
+
 }
